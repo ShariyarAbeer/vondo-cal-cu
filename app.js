563d30ff58c63ca35d57703e2e073355d21b31c7
ShariyarAbeer <007abeershariyar@gmail.com>
2021-08-01 20:53:56 +0600

    vondo percentage done

---
+++ app.js
@@ -0,0 +1,39 @@
+// let value;
+// let num1 = 10;
+// let num2 = 5;
+// console.log(num1 + num2);
+// console.log(num1 * num2);
+// console.log(num1 - num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+// let k = Math.PI;
+// k = Math.E;
+// k = Math.round(7.6);
+// k = Math.ceil(7.1);
+// k = Math.floor(2.9);
+// k = Math.sqrt(16);
+// k = Math.pow(2, 3);
+// k = Math.abs(-5);
+// k = Math.min(2, 5, 12, 45, 4, 7, 56, 7);
+// k = Math.max(2, 5, 12, 45, 4, 7, 56, 7);
+// k = Math.random();
+// console.log(k);
+
+// number = Math.random() * 100;
+// number = Math.floor(number) +1; // cz main 100 pawa jay nah 
+// console.log(number);
+
+
+// Vondo Cal_Cu
+//persons Name
+let firstName = prompt('What is Your name?');
+let partnerName = prompt('What is Your name?');
+
+console.log(firstName, partnerName);
+
+//Calculation
+let number = Math.random() * 100;
+number = Math.floor(number) + 1;
+//Showing result
+alert(firstName + " and " + partnerName + "'s vondo percentage is: " + number + "%")
